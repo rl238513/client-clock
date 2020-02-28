8a20b2a95b793924e247d48eebba5f56a7873e5c
Eduardo Mendoza <rl238513@stu.rlas-116.org>
2020-02-28 13:15:05 -0600

    test 2

--- client clock/TimerViewController.swift
+++ client clock/TimerViewController.swift
@@ -14,6 +14,7 @@ class TimerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
     var hour:Int = 0
     var minute:Int = 0
     var second:Int = 0
+    var test = 1
     
      override func viewDidLoad() {
             super.viewDidLoad()
